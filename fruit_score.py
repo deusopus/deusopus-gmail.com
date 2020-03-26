a58bacf52f2596a98d6cbd64f3ee21b087c19581
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-26 15:22:17 -0700

    Update fruit_score.py

--- fruit_score.py
+++ fruit_score.py
@@ -4,7 +4,7 @@
 # If you lose all your points, "GAME OVER"
 # Enter "exit" at any time to quit
 
-print ("Welcome to 'Fruit Score'")
+print ("Welcome to 'Fruit Score' by deusopus")
 print ("Guess a fruit on my list and earn a point")
 print ("Guess incorrectly and lose a point")
 print ("'Game Over' when you lose all your points")
@@ -13,7 +13,7 @@ print ("Enjoy!")
 
 def fruit_score():
   score = 0
-  fruits = ['apple', 'pear', 'banana', 'orange', 'lemon', 'lime', 'plum', 'grapes', 'kiwi', 'dragonfruit', 'cherries', 'grapefruit', 'guava', 'mango', 'papaya']
+  fruits = ['apple', 'pear', 'banana', 'orange', 'lemon', 'lime', 'plum', 'grapes', 'kiwi', 'dragonfruit', 'cherries', 'grapefruit', 'guava', 'mango', 'papaya', 'pomegranate', 'blueberries', 'pineapple', 'strawberries', 'watermelon']
   while score >= 0:
     fruit = input("Guess a fruit: ")
     if fruit == "exit":
