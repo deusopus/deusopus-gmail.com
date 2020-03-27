05a29d90e59492d37e2d05a2d80ae25b593a0b47
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-27 01:07:37 -0700

    Update guess_again.py

--- guess_again.py
+++ guess_again.py
@@ -1,7 +1,7 @@
 # 'Guess Again' by Brent K Kohler AKA deusopus
 def guess_again():
   import random
-  n = random.randrange(1,10)
+  n = random.randrange(1,11)
   x = 0
   print("Guess a number between 1 and 10")
   while True:
