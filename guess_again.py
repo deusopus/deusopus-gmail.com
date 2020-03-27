8c875ca17f3215fa4affd989567a03c65ad3de10
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-26 22:01:06 -0700

    Update guess_again.py

--- guess_again.py
+++ guess_again.py
@@ -1,18 +1,20 @@
 # 'Guess Again' by Brent K Kohler AKA deusopus
-import random
-n = random.randrange(10)
-x = 0
-print("Guess a number between 1 and 10")
-while True:
-  i = int(input("Guess: "))
-  if i == n:
-    print("Correct")
-    break
-  if x == 2:
-    print("Sorry")
-    print("Game Over")
-    print(f"The number was {n}")
-    break
-  else:
-    print("Wrong")
-    x += 1
+ def guess_again():
+  import random
+  n = random.randrange(11)
+  x = 0
+  print("Guess a number between 1 and 10")
+  while True:
+    i = int(input("Guess: "))
+    if i == n:
+      print("Correct")
+      break
+    if x == 2:
+      print("Sorry")
+      print("Game Over")
+      print(f"The number was {n}")
+      break
+    else:
+      print("Wrong")
+      x += 1
+guess_again()
