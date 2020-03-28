7c21aa2cab4efdb1491671b45fdde679489d94c1
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-27 18:43:15 -0700

    Update guess_again.py

--- guess_again.py
+++ guess_again.py
@@ -1,16 +1,17 @@
 # 'Guess Again' by Brent K Kohler AKA deusopus
+
 def guess_again():
   import random
   n = random.randrange(1,6)
-  t = 0
+  x = 0
   print("Guess a number between 1 and 5")
   while True:
     i = int(input("Guess: "))
-    if i == num:
-      print("You Win!")
+    if i == n:
+      print("Correct!")
       print(f"The number was {n}")
       break
-    if t == 2:
+    if x == 2:
       print("Sorry...")
       print("Game Over")
       print(f"The number was {n}")
@@ -18,12 +19,12 @@ def guess_again():
     else:
       print("Wrong")
       print("Try again")
-      t += 1
+      x += 1
   return
 guess_again()
 
 while True:
-  x = input("Press 'Enter' to play again. Enter 'exit' to quit: ")
+  x = input("Press 'enter' to play again. Type 'exit' to quit: ")
   if x.lower() != "exit":
     guess_again()
   else:
