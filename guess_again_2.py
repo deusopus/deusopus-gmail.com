23213e9bd8851bd37df4c4379bdc15fa47598743
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-27 18:33:13 -0700

    Create guess_again_2.py

---
+++ guess_again_2.py
@@ -0,0 +1,36 @@
+# 'Guess Again' by Brent K Kohler AKA deusopus
+s = 0
+def guess_again(s):
+  import random
+  n = random.randrange(1,6)
+  t = 0
+  print('Guess a number between 1 and 5')
+  print(n)
+  while True:
+    i = int(input("Guess: "))
+    if i == n:
+      print('You Win!')
+      print(f'The number was {n}')
+      s += 1
+      print(f'Score: {s}')
+      break
+    if t == 2:
+      print('Sorry...')
+      print('Game Over')
+      print(f'The number was {n}')
+      break
+    else:
+      print('Wrong')
+      print('Try again')
+      t += 1
+  return s
+guess_again(s)
+
+while True:
+  s = guess_again(s)
+  x = input("Press 'enter' to play again. Type 'exit' to quit: ")
+  if x.lower() != 'exit':
+    guess_again(s)
+  else:
+    print('Thanks for playing!')
+    break
