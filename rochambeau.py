18399f2cbcea73d13df2613b7ad9bc08ca738a85
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-29 16:39:18 -0700

    Update rochambeau.py

--- rochambeau.py
+++ rochambeau.py
@@ -1,29 +1,67 @@
 # 'Rochambeau' by Brent K Kohler AKA deusopus
 # UNDER CONSTRUCTION
-
-import random
-choices = [rock, paper, scissors]
-player = input(str("Choose: (rock, paper, or scissors)"
-c = random.choice(choices)
-print (c)
-if player.lower().strip() == "rock":
-  if computer == "rock":
-    print("tie")
-  if computer == "scissors":
-    print("lose")
-  if computer == "paper"
-    print("win")
-if player.lower().strip() == "paper":
-  if computer == "paper":
-    print("tie")
-  if computer == "rock":
-    print("lose")
-  if computer == "scissors"
-    print("win")
-if player.lower().strip() == "scissors":
-  if computer == "scissors":
-    print("tie")
-  if computer == "paper":
-    print("lose")
-  if computer == "rock"
-    print("lose")    
+t = 0
+w = 0
+l = 0
+def score():
+  print(f"Turns: {t}")
+  print(f"Wins: {w}")
+  print(f"Losses: {l}")
+  return
+def rochambeau():
+  global t
+  global w
+  global l
+  import random
+  choices = ['rock', 'paper', 'scissors']
+  while True:
+    print("Rock, Paper, or Scissors?")
+    p_c = str(input("Make your choice: "))
+    if p_c == exit:
+      quit()
+    c_c = random.choice(choices)
+    print (c_c.title())
+    if p_c.lower().strip() == "rock":
+      if c_c == "rock":
+        print("Draw")
+        score()
+      if c_c == "paper":
+        print("You lose")
+        t += 1
+        l += 1
+        score()
+      if c_c == "scissors":
+        print("You win!")
+        t += 1
+        w += 1
+        score()
+    if p_c.lower().strip() == "paper":
+      if c_c == "paper":
+        print("Draw")
+        score()
+      if c_c == "scissors":
+        print("You lose")
+        t += 1
+        l += 1
+        score()
+      if c_c == "rock":
+        print("You win!")
+        t += 1
+        w += 1
+        score()
+    if p_c.lower().strip() == "scissors":
+      if c_c == "scissors":
+        print("Draw")
+        score()
+      if c_c == "rock":
+        print("You lose")
+        t += 1
+        l += 1
+        score()
+      if c_c == "paper":
+        print("You win!")
+        t += 1
+        w += 1
+        score()    
+  return
+rochambeau()
