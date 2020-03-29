d247e89d6ea1106d8309e427bfc3abe2770b083a
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-29 14:32:10 -0700

    Create rochambeau.py

---
+++ rochambeau.py
@@ -0,0 +1,29 @@
+# 'Rochambeau' by Brent K Kohler AKA deusopus
+# UNDER CONSTRUCTION
+
+import random
+choices = [rock, paper, scissors]
+player = input(str("Choose: (rock, paper, or scissors)"
+c = random.choice(choices)
+print (c)
+if player.lower().strip() == "rock":
+  if computer == "rock":
+    print("tie")
+  if computer == "scissors":
+    print("lose")
+  if computer == "paper"
+    print("win")
+if player.lower().strip() == "paper":
+  if computer == "paper":
+    print("tie")
+  if computer == "rock":
+    print("lose")
+  if computer == "scissors"
+    print("win")
+if player.lower().strip() == "scissors":
+  if computer == "scissors":
+    print("tie")
+  if computer == "paper":
+    print("lose")
+  if computer == "rock"
+    print("lose")    
