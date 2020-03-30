8c95b99f02bdbc9c94ce7444f1a66dd28d8795e2
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-30 16:34:04 -0700

    Update guess_again.py

--- guess_again.py
+++ guess_again.py
@@ -11,50 +11,54 @@ def score():
   return
 def play_again():
   x = input("Press 'enter' to play again. Type 'exit' to quit: ")
-  if x.lower() != "exit":
-    guess_again()
-  else: 
-    print("Thanks for playing!")
+  if x.lower().strip() == "exit":
+    print("Thanks for playing")
     quit()
+  else: 
+    guess_again()
   return
 def instructions():
   print("\n")
   print("I am The Great Enigma!")
   print("I am thinking of a number between 1 and 5...")
-  print("You have three chances to guess correctly.")
-  print("Guess correctly and earn a point.")
-  print("Guess incorrectly and lose your points.")
-  print("Earn 5 points and you win the game.")
+  print("You have three chances to guess correctly")
+  print("Guess correctly and earn a point")
+  print("Guess incorrectly and lose your points")
+  print("Earn 5 points and you win the game")
+  print("Type 'exit' at any time to quit")
   print("\n")
   p = input("Would you like to play?: (Y/n) ")
   if p.lower().strip() == "n":
+    print("Thanks anyway")
     print("Goodbye")
     quit()
   return
 instructions()
 def guess_again():
   import random
-  global s
   n = random.randrange(1,6)
   t = 0
+  global s
   while True:
     score()
-    i = int(input("Guess: "))
-    if i == n:
+    i = input("Guess: ")
+    if i.lower().strip() == "exit":
+      print("Thanks for playing!")
+      print("Goodbye")
+      quit()
+    if int(i) == n:
       print("Correct!")
       print(f"The secret number was {n}")
       s += 1
-      score()
-      play_again()
-    elif i < n:
+      guess_again()
+    elif int(i) < n:
       print("Your guess is too low")
-    elif i > n:
+    elif int(i) > n:
       print("Your guess is too high")
     if t == 2:
       print("Sorry...")
-      print("GAME OVER")
+      print("Game Over")
       print(f"The secret number was {n}")
-      score()
       s = 0
       play_again()
     else:
