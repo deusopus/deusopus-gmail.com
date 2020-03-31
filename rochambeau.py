e0ef1c20dbc4f9ad6877486a1890f4313e936eaf
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-30 17:18:26 -0700

    Update rochambeau.py

--- rochambeau.py
+++ rochambeau.py
@@ -68,7 +68,6 @@ def rochambeau():
   choices = ['rock', 'paper', 'scissors']
   while True:
     c_c = random.choice(choices)
-    print(c_c)
     print ("Rock, Paper, Scissors")
     p_c = input("Go: ")
     if p_c.lower().strip() == "exit":
