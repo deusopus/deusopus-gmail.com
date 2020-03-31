63efba60791c0f13230ad31b2a503e1ef34de9a2
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-31 12:28:00 -0700

    Update deck_dealer.py

--- deck_dealer.py
+++ deck_dealer.py
@@ -30,50 +30,8 @@ def loop():
   return
 def deck_dealer():     
   import random
-  n1 = random.randrange(1,23)
-  if n1 == 1:
-    print("Ace")
-  elif n1 == 2:
-    print("Deuce")
-  elif n1 == 3:
-    print("Cherry")
-  elif n1 == 4:
-    print("Orange")
-  elif n1 == 5:
-    print("Lemon")
-  elif n1 == 6:
-    print("Grape")
-  elif n1 == 7:
-    print("Plum")
-  elif n1 == 8:
-    print("Bell")
-  elif n1 == 9:
-    print("Double Bell")
-  elif n1 == 11:
-    print("Jack")
-  elif n1 == 12:
-    print("Queen")
-  elif n1 == 13:
-    print("King")
-  elif n1 == 14:
-    print("7")
-  elif n1 == 15:
-    print("77")
-  elif n1 == 16:
-    print("777")
-  elif n1 == 17:
-    print("Bar")
-  elif n1 == 18:
-    print("Double Bar")
-  elif n1 == 19:
-    print("Triple Bar")
-  elif n1 == 20:
-    print("Wild")
-  elif n1 == 21:
-    print("Wild")
-  elif n1 == 22:
-    print("Wild")
-  else:
-    print(n1)
+  slots = ['Ace','Deuce','Cherry','Orange','Lemon','Grape','Plum','Bell','Double Bell','Triple Bell','10','Jack','Queen','King','7','77','777','Bar','Double Bar','Triple Bar','Wild','Wild','Wild','Wild']
+  n1 = random.choice(slots)
+  print(n1)
   return
 welcome()
