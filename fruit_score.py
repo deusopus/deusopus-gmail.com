6d5c787792438455f5af508dff4888debc8b75ff
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-31 13:37:10 -0700

    Update fruit_score.py

--- fruit_score.py
+++ fruit_score.py
@@ -4,6 +4,11 @@
 # If you lose all your points, "GAME OVER"
 # Enter "exit" at any time to quit
 
+s = 0
+h = 0
+h1 = 0
+g = 0
+a = 0
 def instructions():
   print("\n")
   print ("Welcome to 'Fruit Score' by deusopus")
@@ -13,61 +18,55 @@ def instructions():
   print ("Type 'exit' at any time to quit")
   print ("\n")
   return
-
+def score():
+  global s
+  print(f"Score: {s}")
+  return
 def fruit_score():
+  global s
+  global h
+  global g
+  global a
   import random
-  score = 0
-  hint = 0
-  good = 0
-  awesome = 0
   instructions()
-  fruit = ['yuzu','tamarind','strawberry','star fruit','soursop','raspberry','quince','pineberry','pineapple','persimmon','pear','peach','passionfruit','papaya','orange','mulberry','watermelon','mango','lime','lemon','kumquat','kiwifruit','plum','jackfruit','guava','grape','gooseberry','fig','dragonfruit','durian','coconut','cherry','blueberry','blackberry','banana','avocado','apricot','apple']
-  old_fruit = [] 
-  while score >= 0:
+  fruit = ['yuzu','tamarind','strawberry','star fruit','soursop','raspberry','quince','pineberry','pineapple','persimmon','pear','peach','passionfruit','papaya','orange','mulberry','watermelon','mango','lime','lemon','kumquat','kiwifruit','plum','jackfruit','guava','grapes','gooseberry','fig','dragonfruit','durian','coconut','cherry','blueberry','blackberry','banana','avocado','apricot','apple']
+  old_fruit = []
+  while True:
+    score()
     guess = input("Guess a fruit: ")
     if guess.lower().strip() == "exit":
-      print(f"Score: {score}")
       print("Goodbye")
       print("Thanks for playing")
       break
-    if guess.lower().strip() in fruit:
-      if guess.lower().strip() not in old_fruit:
-        print("Yes")
-        old_fruit.append(guess)
-        fruit.remove(guess)
-        score += 1
-        good += 1
-        awesome += 1
-        if good == 3:
-          print("Good job!")
-          good = 0
-        if awesome == 5:
-          print("Awesome!")
-          awesome = 0
-        print(f"Score: {score}")
-    else:
+    if guess.lower().strip() not in fruit:
       if guess.lower().strip() in old_fruit:
         print("You already guessed that")
-        print(f"Score: {score}")
-        hint += 1
-        if hint == 2:
-          i = random.choice(fruit)
+        h += 1
+        if h == 2:
           print("Here's a hint")
-          print (i.title())
-          hint = 0
-      else:
-        score -= 1
-        if score < 0:
-          print("Sorry")
+          h1 = random.choice(fruit)
+          print(h1.title())   
+          h = 0
+    if guess.lower().strip() in fruit:
+      print("Yes")
+      fruit.remove(guess)
+      old_fruit.append(guess)
+      s += 1
+      g += 1
+      a += 1
+      if g == 3:
+        print("Good job!")
+        g = 0
+      if a == 5:
+        print("Awesome!")
+        a = 0
+    if guess.lower().strip() not in fruit:
+      if guess.lower().strip() not in old_fruit:
+        s -= 1
+        if s <= 0:
           print("Score: 0")
           print("Game Over")
-          quit()
+          break
         else:
-          print("Sorry")
-          print(f"Score: {score}")
-        if score <= 0:
-          print("You lost all your points")
-          print("Game Over")
-          break     
-  return
+          print("Try again")
 fruit_score()
