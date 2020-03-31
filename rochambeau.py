2e891e012bff35c88ca1b3a410f2f0cef3b4ff9a
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-30 17:10:27 -0700

    Update rochambeau.py

--- rochambeau.py
+++ rochambeau.py
@@ -9,8 +9,8 @@ def score():
   print(f"Losses: {l}")
   if t == 2:
     if w == 2:
-      print("You proved it!")
-      print("You get dibs")
+      print("You win!")
+      print("Goodbye")
       quit()
   if t == 2:
     if w == 0:
@@ -19,14 +19,25 @@ def score():
         print("You lose")
         quit()
   if t == 3:
+    if w == 2:
+      if l == 1:
+        print("You win!")
+        print("Goodbye")
+        quit()   
+  if t == 3:
     if w == 1:
       if l == 2:
         if c < 1:
-          x = str(input("Would you like to go for 3 out of 5?: (y/n)"))
+          x = input("Try for best 3 out of 5?: (Y/n)")
           if x.lower().strip() == "n":
             print("Better luck next time")
             print("Goodbye")
             quit()
+  if t == 4:
+    if w == 1:
+      print("Sorry...")
+      print("You lose")
+      quit()
   if t == 5:
     if w <= 2:
       if l >= 3:
@@ -36,8 +47,8 @@ def score():
   if t == 5:
     if w == 3:
       if l == 2:
-        print("You proved it!")
-        print("You get dibs")
+        print("You win!")
+        print("Goodbye")
         quit()
   return
 def instructions():
@@ -48,17 +59,18 @@ def instructions():
   return
 def rochambeau():
   instructions()
+  score()
   global t
   global w
   global l
   global c
   import random
   choices = ['rock', 'paper', 'scissors']
-  score()
   while True:
     c_c = random.choice(choices)
+    print(c_c)
     print ("Rock, Paper, Scissors")
-    p_c = str(input("Go: "))
+    p_c = input("Go: ")
     if p_c.lower().strip() == "exit":
       quit()
     print ("I choose", c_c.title())
@@ -103,6 +115,12 @@ def rochambeau():
         print("You win!")
         t += 1
         w += 1
-        score()    
-  return
+        score() 
+    else:
+      if p_c.lower().strip() != "rock":
+        if p_c.lower().strip() != "paper":
+          if p_c.lower().strip() != "scissors":
+            print("Invalid choice")
+            print("Try again")
+    
 rochambeau()
