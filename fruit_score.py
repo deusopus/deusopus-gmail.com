ebf95cb6731e0da584481dd48b6596db80b2004d
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-31 22:13:16 -0700

    Update fruit_score.py

--- fruit_score.py
+++ fruit_score.py
@@ -3,12 +3,10 @@
 # Guess incorrectly and lose a point
 # If you lose all your points, "GAME OVER"
 # Enter "exit" at any time to quit
-
-s = 0
-h = 0
-h1 = 0
+score = 0
 g = 0
 a = 0
+h = 0
 def instructions():
   print("\n")
   print ("Welcome to 'Fruit Score' by deusopus")
@@ -18,21 +16,13 @@ def instructions():
   print ("Type 'exit' at any time to quit")
   print ("\n")
   return
-def score():
-  global s
-  print(f"Score: {s}")
-  return
-def fruit_score():
-  global s
-  global h
-  global g
-  global a
+def fruit_score(score,g,a,h):
   import random
   instructions()
   fruit = ['yuzu','tamarind','strawberry','star fruit','soursop','raspberry','quince','pineberry','pineapple','persimmon','pear','peach','passionfruit','papaya','orange','mulberry','watermelon','mango','lime','lemon','kumquat','kiwifruit','plum','jackfruit','guava','grapes','gooseberry','fig','dragonfruit','durian','coconut','cherry','blueberry','blackberry','banana','avocado','apricot','apple']
   old_fruit = []
   while True:
-    score()
+    score_keeper(score,g,a,h)
     guess = input("Guess a fruit: ")
     if guess.lower().strip() == "exit":
       print("Goodbye")
@@ -51,7 +41,7 @@ def fruit_score():
       print("Yes")
       fruit.remove(guess)
       old_fruit.append(guess)
-      s += 1
+      score += 1
       g += 1
       a += 1
       if g == 3:
@@ -63,12 +53,14 @@ def fruit_score():
     if guess.lower().strip() not in fruit:
       if guess.lower().strip() not in old_fruit:
         print("Sorry")
-        s -= 1
-        if s <= 0:
-          s = 0
-          print(f"Score: {s}")
+        score -= 1
+        if score <= 0:
+          score = 0
+          print(f"Score: {score}")
           print("Game Over")
           break
-        else:
-          print("Try again")
-fruit_score()
+  return score,g,a,h
+def score_keeper(score,g,a,h):
+  print(f"Score: {score}")
+  return (score,g,a,h)
+fruit_score(score,g,a,h)
