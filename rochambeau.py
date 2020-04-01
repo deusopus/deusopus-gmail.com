19c793840782382afb2e4d74bd7e75334d8dcf62
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-31 22:15:08 -0700

    Update rochambeau.py

--- rochambeau.py
+++ rochambeau.py
@@ -2,10 +2,7 @@
 t = 0
 w = 0
 l = 0
-def play_again():
-  global t
-  global w
-  global l
+def play_again(t,w,l):
   y = input("Would you like to play again?: (Y/n)")
   if y.lower().strip() == "n":
     print("See you next time")
@@ -15,12 +12,9 @@ def play_again():
     t = 0
     w = 0
     l = 0
-    rochambeau()
-  return
-def score():
-  global t
-  global w
-  global l
+    rochambeau(t,w,l)
+  return t,w,l
+def score(t,w,l):
   print(f"Turns: {t}")
   print(f"Wins: {w}")
   print(f"Losses: {l}")
@@ -28,7 +22,7 @@ def score():
     if w == 2:
       print("You win!")
       print("Game Over")
-      play_again()
+      play_again(t,w,l)
   if t== 2:
     if w == 1:
       if l == 1:
@@ -38,13 +32,13 @@ def score():
       if l == 2:
         print("Sorry...")
         print("Game Over")
-        play_again()
+        play_again(t,w,l)
   if t == 3:
     if w == 2:
       if l == 1:
         print("You win!")
         print("Goodbye")
-        play_again()
+        play_again(t,w,l)
   if t == 3:
     if w == 1:
       if l == 2:
@@ -58,7 +52,7 @@ def score():
       if l == 3:
         print("Sorry...")
         print("Game Over")
-        play_again()
+        play_again(t,w,l)
   if t == 4:
     if w == 2:
       if l == 2:
@@ -68,14 +62,14 @@ def score():
       if l == 3:
         print("Sorry...")
         print("Game Over")
-        play_again()     
+        play_again(t,w,l)     
   if t == 5:
     if w == 3:
       if l == 2:
         print("You win!")
         print("Game Over")
-        play_again()
-  return
+        play_again(t,w,l)
+  return t,w,l
 def instructions():
   print("\n")
   print("Welcome to 'Rochambeau' by deusopus")
@@ -84,12 +78,9 @@ def instructions():
   print("Have fun!")
   print("\n")
   return
-def rochambeau():
-  global t
-  global w
-  global l
+def rochambeau(t,w,l):
   instructions()
-  score()
+  score(t,w,l)
   import random
   choices = ['rock', 'paper', 'scissors']
   while True:
@@ -106,49 +97,49 @@ def rochambeau():
     if p_c.lower().strip() == "rock":
       if c_c == "rock":
         print("Draw")
-        score()
+        score(t,w,l)
       if c_c == "paper":
         print("You lose")
         t += 1
         l += 1
-        score()
+        score(t,w,l)
       if c_c == "scissors":
         print("You win!")
         t += 1
         w += 1
-        score()
+        score(t,w,l)
     if p_c.lower().strip() == "paper":
       if c_c == "paper":
         print("Draw")
-        score()
+        score(t,w,l)
       if c_c == "scissors":
         print("You lose")
         t += 1
         l += 1
-        score()
+        score(t,w,l)
       if c_c == "rock":
         print("You win!")
         t += 1
         w += 1
-        score()
+        score(t,w,l)
     if p_c.lower().strip() == "scissors":
       if c_c == "scissors":
         print("Draw")
-        score()
+        score(t,w,l)
       if c_c == "rock":
         print("You lose")
         t += 1
         l += 1
-        score()
+        score(t,w,l)
       if c_c == "paper":
         print("You win!")
         t += 1
         w += 1
-        score() 
+        score(t,w,l) 
     else:
       if p_c.lower().strip() != "rock":
         if p_c.lower().strip() != "paper":
           if p_c.lower().strip() != "scissors":
             print("Invalid choice")
             print("Try again")
-rochambeau()
+rochambeau(t,w,l)
