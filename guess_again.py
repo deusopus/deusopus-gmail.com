4341ba3538bfd7718a093224389c7a1c24da6109
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-03-31 22:13:52 -0700

    Update guess_again.py

--- guess_again.py
+++ guess_again.py
@@ -1,22 +1,7 @@
 # 'Guess Again' by Brent Kaimanu Kohler AKA deusopus
-s = 0
-def score():
-  print(f"Score: {s}")
-  if s == 5:
-    print("Congratulations.")
-    print("You win!")
-    print("You have unlocked the secret...")
-    print("Goodbye")
-    quit()    
-  return
-def play_again():
-  x = input("Press 'enter' to play again. Type 'exit' to quit: ")
-  if x.lower().strip() == "exit":
-    print("Thanks for playing")
-    quit()
-  else: 
-    guess_again()
-  return
+turns = 0
+score = 0
+n1 = 0
 def instructions():
   print("\n")
   print("I am The Great Enigma!")
@@ -33,36 +18,56 @@ def instructions():
     print("Goodbye")
     quit()
   return
-instructions()
-def guess_again():
+def guess_again(score,turns,n1):
   import random
-  n = random.randrange(1,6)
-  t = 0
-  global s
+  n1 = random.randrange(1,6)
   while True:
-    score()
-    i = input("Guess: ")
-    if i.lower().strip() == "exit":
+    score_keeper(score,turns,n1)
+    n = input("Guess: ")
+    if n.lower().strip() == "exit":
       print("Thanks for playing!")
       print("Goodbye")
-      quit()
-    if int(i) == n:
+      break
+    if int(n) == n1:
       print("Correct!")
-      print(f"The secret number was {n}")
-      s += 1
-      guess_again()
-    elif int(i) < n:
+      print(f"The secret number was {n1}")
+      score += 1
+      turns = 0  
+    if int(n) < n1:
       print("Your guess is too low")
-    elif int(i) > n:
+      turns += 1
+      if turns > 2:
+        print("Sorry...")
+        print("Game Over")
+        print(f"The secret number was {n1}") 
+        turns = 0
+        play_again(score,turns,n1)
+    if int(n) > n1:
       print("Your guess is too high")
-    if t == 2:
-      print("Sorry...")
-      print("Game Over")
-      print(f"The secret number was {n}")
-      s = 0
-      play_again()
-    else:
-      print("Try again")
-      t += 1
-  return
-guess_again()
+      turns += 1
+      if turns > 2:
+        print("Sorry...")
+        print("Game Over")
+        print(f"The secret number was {n1}")
+        turns = 0 
+        play_again(score,turns,n1)
+  return (score,turns,n1)  
+def score_keeper(score,turns,n1):
+  print(f"Score: {score}")
+  if score == 5:
+    print("Congratulations")
+    print("You win!")
+    print("You have unlocked the secret...")
+    print("Goodbye")
+    quit()    
+  return score,turns,n1
+def play_again(score,turns,n1):
+  x = input("Press 'enter' to play again. Type 'exit' to quit: ")
+  if x.lower().strip() == "exit":
+    print("Thanks for playing")
+    quit()
+  else: 
+    guess_again(score,turns,n1)
+  return score,turns,n1
+instructions()
+guess_again(score,turns,n1)
