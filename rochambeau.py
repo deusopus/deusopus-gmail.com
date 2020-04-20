a53d402d3771b5780aebdde8c4357ce613cd4833
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-04-19 23:04:46 -0700

    Update rochambeau.py

--- rochambeau.py
+++ rochambeau.py
@@ -10,6 +10,7 @@ def play_again(t,w,l,c):
     t = 0
     w = 0
     l = 0
+    c = 0
     rochambeau(t,w,l,c)
   return t,w,l,c
 def score(t,w,l,c):
@@ -25,28 +26,23 @@ def score(t,w,l,c):
     if w == 1:
       if l == 1:
         print("Here's the tie-breaker...")
-  if t == 2:
-    if w == 0:
+  if t == 3:
+    if w == 1:
       if l == 2:
-        print("Sorry...")
-        print("Game Over")
-        play_again(t,w,l,c)
+        if c == 0:
+          x = input("Try for best 3 out of 5?: (Y/n)")
+          if x.lower().strip() == "n":
+            print("Thanks for trying")
+            print("Game Over")
+            quit()
+          else:
+            c += 1
   if t == 3:
     if w == 2:
       if l == 1:
         print("You win!")
         print("Goodbye")
         play_again(t,w,l,c)
-  if t == 3:
-    if w == 1:
-      if l == 2:
-        if c < 1:
-          c += 1
-          x = input("Try for best 3 out of 5?: (Y/n)")
-          if x.lower().strip() == "n":
-            print("Thanks for trying")
-            print("Game Over")
-            quit()
   if t == 4:
     if w == 1:
       if l == 3:
@@ -71,12 +67,10 @@ def score(t,w,l,c):
         play_again(t,w,l,c)
   return t,w,l,c
 def instructions():
-  print("\n")
   print("Welcome to 'Rochambeau' by deusopus")
   print("The age-old game of Rock, Paper, Scissors")
   print("Type 'exit' at any time to quit")
   print("Have fun!")
-  print("\n")
   return
 def rochambeau(t,w,l,c):
   instructions()
@@ -85,16 +79,14 @@ def rochambeau(t,w,l,c):
   choices = ['rock', 'paper', 'scissors']
   while True:
     c_c = random.choice(choices)
-    print ("\n")
-    print ("Rock, Paper, Scissors")
+    print ("(R)ock, (P)aper, (S)cissors")
     p_c = input("Go!: ")
     if p_c.lower().strip() == "exit":
       print("Thanks for playing")
       print("Goodbye")
       quit()
     print ("I choose", c_c.title())
-    print ("\n")
-    if p_c.lower().strip() == "rock":
+    if p_c.lower().strip() == "r":
       if c_c == "rock":
         print("Draw")
         score(t,w,l,c)
@@ -108,7 +100,7 @@ def rochambeau(t,w,l,c):
         t += 1
         w += 1
         score(t,w,l,c)
-    if p_c.lower().strip() == "paper":
+    if p_c.lower().strip() == "p":
       if c_c == "paper":
         print("Draw")
         score(t,w,l,c)
@@ -122,7 +114,7 @@ def rochambeau(t,w,l,c):
         t += 1
         w += 1
         score(t,w,l,c)
-    if p_c.lower().strip() == "scissors":
+    if p_c.lower().strip() == "s":
       if c_c == "scissors":
         print("Draw")
         score(t,w,l,c)
@@ -137,9 +129,9 @@ def rochambeau(t,w,l,c):
         w += 1
         score(t,w,l,c) 
     else:
-      if p_c.lower().strip() != "rock":
-        if p_c.lower().strip() != "paper":
-          if p_c.lower().strip() != "scissors":
+      if p_c.lower().strip() != "r":
+        if p_c.lower().strip() != "p":
+          if p_c.lower().strip() != "s":
             print("Invalid choice")
             print("Try again")
 rochambeau(t,w,l,c)
