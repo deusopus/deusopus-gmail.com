24e589a2e0298257388b4c263b14e7d96ca59605
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-06-17 02:07:51 -0700

    Update dnd_abcalc.py

--- dnd_abcalc.py
+++ dnd_abcalc.py
@@ -1,17 +1,23 @@
 #Authentic D&D Ability Calculator by deusopus
 print('Authentic D&D Ability Calculator by deusopus')
+print()
 import random
 d6a = []
+drawer = {}
 traits = ['Strength', 'Dexterity', 'Constitution', 'Intelligence', 'Wisdom', 
          'Charisma']
 for rolla in range(len(traits)):
   for rollb in range(4):
     d6 = random.randrange(1,7)
     d6a.append(d6)  
-  low = (min(d6a))
-  d6a.remove(min(d6a))
   print(f'Roll {rolla+1}')
+  low = (min(d6a))
+  d6a.remove(min(d6a)) 
   print(f'{d6a} Low:{low}')
   total = sum(d6a)
   print(f'{traits[rolla]}: {total}/18')
+  print()   
+  drawer[rolla] = d6a
   d6a = []
+for key,value in drawer.items():
+  print(key, ":", value) 
