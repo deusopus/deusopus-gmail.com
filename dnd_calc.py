248a660da7f5e01c3d45f36dfbc4317da0f3e8b8
Brent Kohler AKA deusopus <deusopus@gmail.com>
2020-06-24 21:00:09 -0700

    Update and rename dnd_abcalc.py to dnd_calc.py

--- dnd_calc.py
+++ dnd_calc.py
@@ -10,13 +10,13 @@ for rolla in range(len(traits)):
   for rollb in range(4):
     d6 = random.randrange(1,7)
     d6a.append(d6)  
-# print(f'Roll {rolla+1}')
+  print(f'Roll {rolla+1}')
   low = (min(d6a))
   d6a.remove(min(d6a)) 
-#  print(f'{d6a} Low:{low}')
+  print(f'{d6a} Low:{low}')
   total = sum(d6a)
-#  print(f'{traits[rolla]}: {total}/18')
-#  print()   
+  print(f'{traits[rolla]}: {total}/18')
+  print()   
   drawer[traits[rolla]] = d6a
   d6a = []
 for key,value in drawer.items():
