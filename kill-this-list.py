59e41b2b1612a3d659fb1bbf2732b6882c36f17f
Grass Shack Software <brent@grass-shack-studios.com>
2021-10-12 00:01:28 -0700

    Update kill-this-list.py

--- kill-this-list.py
+++ kill-this-list.py
@@ -1,14 +1,15 @@
+import random
 from random import randrange
+
 while True:
-  row = []
 
-  for number in range(1,11):
-    row.append(randrange(1,11))
+    lst = []
+    for item in range(0,10):
+        while len(lst) < 11:
+            lst.append(randrange(0,7))
+            print(lst)
 
-  while len(row) > 0:
-    random.shuffle(row)
-    print(row)
-    n = int(input('Which number to remove? '))
-    row = [i for i in row if i != n]
-    if n != (row):
-      print('Try again')
+    for item in range(0,len(lst)):
+        while len(lst) > 1:
+            lst.remove(random.choice(lst))
+            print(lst)
